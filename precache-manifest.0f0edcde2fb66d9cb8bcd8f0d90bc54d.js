89b0d4671af62d88801178381c7e293c0d76be0b
Denys Kulibaba <den.kulibab@gmail.com>
2020-11-17 15:31:33 +0200

    Updates

---
+++ precache-manifest.0f0edcde2fb66d9cb8bcd8f0d90bc54d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b352b167b7d05de28ef6d0b102557671",
+    "url": "/test_task/index.html"
+  },
+  {
+    "revision": "0903eba7c9d99ffb8b48",
+    "url": "/test_task/static/css/main.1022401b.chunk.css"
+  },
+  {
+    "revision": "98c588752d8a6dc102e3",
+    "url": "/test_task/static/js/2.45c50669.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/test_task/static/js/2.45c50669.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0903eba7c9d99ffb8b48",
+    "url": "/test_task/static/js/main.dfb58497.chunk.js"
+  },
+  {
+    "revision": "eff5173cee1c2aba7e6f",
+    "url": "/test_task/static/js/runtime-main.97b990d2.js"
+  }
+]);
